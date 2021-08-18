7b611dddd8b2c33253b55ae8e404b5169bf1b68f
Link Wu <coin12.wu@gmail.com>
2021-08-18 17:33:48 +0800

    update Open API auth method

--- node/index.js
+++ node/index.js
@@ -10,17 +10,19 @@ const API_KEY_SECRET = process.env.API_KEY_SECRET;
 const API_MODE = process.env.API_MODE || 'sandbox';
 
 // Configuration
-const SMILE_API_HOST = (API_MODE == 'sandbox') ? 'open-sandbox.smileapi.io' : 'open.smileapi.io';
+const SMILE_API_HOST = (API_MODE == 'sandbox') ? 'https://open-sandbox.smileapi.io' : 'https://open.smileapi.io';
+const SMILE_OPEN_API_SIGNATURE = Buffer.from(API_KEY_ID + ':' + API_KEY_SECRET).toString('base64');
 
 // App
 const app = express();
 app.use(cors());
 app.use(express.static('../frontend'));
-app.post('/api/create_link_token', async(request, response, next) => {
+app.get('/api/create_link_token', async(request, response, next) => {
     const post = bent(SMILE_API_HOST, 'POST', 'json', 200);
-    const createUserResult = await post('users', {});
+    const createUserResult = await post('/users', {}, { "Authorization": "Basic " + SMILE_OPEN_API_SIGNATURE });
     const userId = createUserResult.data.id;
-    const createTokenResult = await post('tokens' + userId, { userId });
+    console.log(createUserResult, userId);
+    const createTokenResult = await post('/tokens' + userId, {}, { "Authorization": "Basic " + SMILE_OPEN_API_SIGNATURE });
     const userToken = createTokenResult.data;
     response.json(userToken);
 });
