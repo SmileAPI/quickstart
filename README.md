ebcaa89f82c7fa81de489ddd3e0619f9c9fe639f
chengang <chengang@xiaoda.org>
2021-08-20 14:46:01 +0800

    fix to get connected

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Smile Quick Start
 
 ## Node.js Backend Usage
-
-1. Run following codes:
+1. Create a new .env file under node directory, set API_KEY_ID, API_KEY_SECRET and Mode.
+2. Run following codes:
 ```
 cd node
 node index.js
 ```
-2. Open http://127.0.0.1:8000 to test
-3. For more configuration, see `node/.env`
+3. Open http://127.0.0.1:8000 to test
+4. For more configuration, see `node/.env`
--- node/index.js
+++ node/index.js
@@ -2,28 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bent = require('bent');
+const getJSON = bent('json')
 
 // Environment
 const APP_PORT = process.env.APP_PORT || 8000;
 const API_KEY_ID = process.env.API_KEY_ID;
 const API_KEY_SECRET = process.env.API_KEY_SECRET;
-const API_MODE = process.env.API_MODE || 'sandbox';
+const SMILE_API_HOST = process.env.API_HOST || 'https://sandbox.smileapi.io';
 
 // Configuration
-const SMILE_API_HOST = (API_MODE == 'sandbox') ? 'https://open-sandbox.smileapi.io' : 'https://open.smileapi.io';
 const SMILE_OPEN_API_SIGNATURE = Buffer.from(API_KEY_ID + ':' + API_KEY_SECRET).toString('base64');
 
 // App
 const app = express();
 app.use(cors());
 app.use(express.static('../frontend'));
-app.get('/api/create_link_token', async(request, response, next) => {
-    const post = bent(SMILE_API_HOST, 'POST', 'json', 200);
-    const createUserResult = await post('/users', {}, { "Authorization": "Basic " + SMILE_OPEN_API_SIGNATURE });
-    const userId = createUserResult.data.id;
-    console.log(createUserResult, userId);
-    const createTokenResult = await post('/tokens' + userId, {}, { "Authorization": "Basic " + SMILE_OPEN_API_SIGNATURE });
-    const userToken = createTokenResult.data;
+app.get('/api/create_link_token', async (request, response, next) => {
+
+    const post = bent(SMILE_API_HOST, 'POST', 'json', 201);
+    const createUserResult = await post('/users', {}, {"Authorization": "Basic " + SMILE_OPEN_API_SIGNATURE});
+    const userToken = createUserResult.data.token;
+    console.log('userToken:', userToken);
     response.json(userToken);
 });
 app.listen(APP_PORT, () => {
