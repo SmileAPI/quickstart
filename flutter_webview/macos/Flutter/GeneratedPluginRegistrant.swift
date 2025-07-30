c7a9aec5f87a8c03ea69a8bfdef628b989280f92
Gavin Zhang <gavin.zhang@getsmileapi.com>
2025-07-30 08:00:02 +0000

    [QuickStart] 🎉feat:Upgrade quick start demos , fixed the jdk, sdk, node versions
    [QuickStart] 🎉feat:Upgrade quick start demos , fixed the jdk, sdk, node versions
    
    1. Upgrade quick start demos , fixed the jdk, sdk, node versions
    
    ref smileapi/awesome-smile#3732

--- flutter_webview/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_webview/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
